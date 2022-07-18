800aa27ab077f241ba95f6a6d116dc153dbb4d5e
Kopaing12 <107101116+Kopaing12@users.noreply.github.com>
2022-07-19 00:24:23 +0630

    Update mdb.py

--- database/mdb.py
+++ database/mdb.py
@@ -6,7 +6,7 @@
 import re
 import pymongo
 
-from pymongo.errors import 
+from pymongo.errors import DuplicateKeyError
 from marshmallow.exceptions import ValidationError
 
 from config import DATABASE_URI, DATABASE_NAME
