b260a383ae9960b77162b582d0dccc2525a3964e
Kopaing12 <107101116+Kopaing12@users.noreply.github.com>
2022-07-19 00:05:09 +0630

    Update connection.py

--- plugins/connection.py
+++ plugins/connection.py
@@ -1,147 +1,89 @@
-from pyrogram import filters, Client
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from database.connections_mdb import add_connection, all_connections, if_active, delete_connection
-from info import ADMINS
-import logging
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.ERROR)
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @trojanzhex
 
 
-@Client.on_message((filters.private | filters.group) & filters.command('connect'))
-async def addconnection(client, message):
-    userid = message.from_user.id if message.from_user else None
-    if not userid:
-        return await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
-    chat_type = message.chat.type
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from script import script
 
-    if chat_type == "private":
-        try:
-            cmd, group_id = message.text.split(" ", 1)
-        except:
-            await message.reply_text(
-                "<b>Enter in correct format!</b>\n\n"
-                "<code>/connect groupid</code>\n\n"
-                "<i>Get your Group id by adding this bot to your group and use  <code>/id</code></i>",
-                quote=True
-            )
-            return
 
-    elif chat_type in ["group", "supergroup"]:
-        group_id = message.chat.id
-
+@Client.on_message(filters.command(["start"]) & filters.private)
+async def start(client, message):
     try:
-        st = await client.get_chat_member(group_id, userid)
-        if (
-                st.status != "administrator"
-                and st.status != "creator"
-                and userid not in ADMINS
-        ):
-            await message.reply_text("You should be an admin in Given group!", quote=True)
-            return
-    except Exception as e:
-        logger.exception(e)
         await message.reply_text(
-            "Invalid Group ID!\n\nIf correct, Make sure I'm present in your group!!",
-            quote=True,
-        )
-
-        return
-    try:
-        st = await client.get_chat_member(group_id, "me")
-        if st.status == "administrator":
-            ttl = await client.get_chat(group_id)
-            title = ttl.title
-
-            addcon = await add_connection(str(group_id), str(userid))
-            if addcon:
-                await message.reply_text(
-                    f"Successfully connected to **{title}**\nNow manage your group from my pm !",
-                    quote=True,
-                    parse_mode="md"
-                )
-                if chat_type in ["group", "supergroup"]:
-                    await client.send_message(
-                        userid,
-                        f"Connected to **{title}** !",
-                        parse_mode="md"
-                    )
-            else:
-                await message.reply_text(
-                    "You're already connected to this chat!",
-                    quote=True
-                )
-        else:
-            await message.reply_text("Add me as an admin in group", quote=True)
-    except Exception as e:
-        logger.exception(e)
-        await message.reply_text('Some error occurred! Try again later.', quote=True)
-        return
-
-
-@Client.on_message((filters.private | filters.group) & filters.command('disconnect'))
-async def deleteconnection(client, message):
-    userid = message.from_user.id if message.from_user else None
-    if not userid:
-        return await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
-    chat_type = message.chat.type
-
-    if chat_type == "private":
-        await message.reply_text("Run /connections to view or disconnect from groups!", quote=True)
-
-    elif chat_type in ["group", "supergroup"]:
-        group_id = message.chat.id
-
-        st = await client.get_chat_member(group_id, userid)
-        if (
-                st.status != "administrator"
-                and st.status != "creator"
-                and str(userid) not in ADMINS
-        ):
-            return
-
-        delcon = await delete_connection(str(userid), str(group_id))
-        if delcon:
-            await message.reply_text("Successfully disconnected from this chat", quote=True)
-        else:
-            await message.reply_text("This chat isn't connected to me!\nDo /connect to connect.", quote=True)
-
-
-@Client.on_message(filters.private & filters.command(["connections"]))
-async def connections(client, message):
-    userid = message.from_user.id
-
-    groupids = await all_connections(str(userid))
-    if groupids is None:
-        await message.reply_text(
-            "There are no active connections!! Connect to some groups first.",
-            quote=True
-        )
-        return
-    buttons = []
-    for groupid in groupids:
-        try:
-            ttl = await client.get_chat(int(groupid))
-            title = ttl.title
-            active = await if_active(str(userid), str(groupid))
-            act = " - ACTIVE" if active else ""
-            buttons.append(
+            text=script.START_MSG.format(message.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
                 [
-                    InlineKeyboardButton(
-                        text=f"{title}{act}", callback_data=f"groupcb:{groupid}:{act}"
-                    )
+                    [
+                        InlineKeyboardButton("HELP", callback_data="help_data"),
+                        InlineKeyboardButton("ABOUT", callback_data="about_data"),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/MKSVIPLINK"),
+                    ],
+                    [
+                        InlineKeyboardButton("⭕️ Group ⭕️ ", url="https://t.me/MKS_RequestGroup")
+                    ]
                 ]
-            )
-        except:
-            pass
-    if buttons:
-        await message.reply_text(
-            "Your connected group details ;\n\n",
-            reply_markup=InlineKeyboardMarkup(buttons),
-            quote=True
+            ),
+            reply_to_message_id=message.message_id
         )
-    else:
+    except:
+        pass
+
+@Client.on_message(filters.command(["help"]) & filters.private)
+async def help(client, message):
+    try:
         await message.reply_text(
-            "There are no active connections!! Connect to some groups first.",
-            quote=True
+            text=script.HELP_MSG,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("BACK", callback_data="start_data"),
+                        InlineKeyboardButton("ABOUT", callback_data="about_data"),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/MKSVIPLINK"),
+                    ],
+                    [
+                        InlineKeyboardButton("⭕️ Group ⭕️ ", url="https://t.me/MKS_RequestGroup")
+                    ]
+                ]
+            ),
+            reply_to_message_id=message.message_id
         )
+    except:
+        pass
+
+@Client.on_message(filters.command(["about"]) & filters.private)
+async def about(client, message):
+    try:
+        await message.reply_text(
+            text=script.ABOUT_MSG,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("BACK", callback_data="help_data"),
+                        InlineKeyboardButton("START", callback_data="start_data"),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/MKSVIPLINK"),
+                        InlineKeyboardButton("⭕️ Group ⭕️ ", url="https://t.me/MKS_RequestGroup"),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "SOURCE CODE", url="https://t.me/kopaing15")
+                    ]
+                ]
+            ),
+            reply_to_message_id=message.message_id
+        )
+    except:
+        pass
