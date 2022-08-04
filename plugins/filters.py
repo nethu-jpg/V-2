66611f053b6b37e682e08e7c64690d6f8295e39a
Kopaing12 <107101116+Kopaing12@users.noreply.github.com>
2022-08-04 12:38:01 +0630

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -41,7 +41,7 @@ async def filter(client: Bot, message: Message):
         if filenames and links:
             for filename, link in zip(filenames, links):
                 btn.append(
-                    [InlineKeyboardButton(text=f"♻️ {filename}",url=f"{link}")]
+                    [InlineKeyboardButton(text=f"💁🏼 {filename}",url=f"{link}")]
             )
            
         else:
@@ -52,7 +52,7 @@ async def filter(client: Bot, message: Message):
 
         if len(btn) > 10: 
             btns = list(split_list(btn, 6)) 
-            keyword = f"♻️ {message.chat.id}-{message.message_id}"
+            keyword = f"💁🏼 {message.chat.id}-{message.message_id}"
             BUTTONS[keyword] = {
                 "total" : len(btns),
                 "buttons" : btns
