17b150704aa40534c6de0bb482e396bd901cc777
Kim So-hyun <110754631+Mr-BUG-TG@users.noreply.github.com>
2022-08-22 21:27:07 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -74,7 +74,7 @@ async def filter(client: Bot, message: Message):
             await client.send_photo(
                 chat_id=message.chat.id,
                 photo=poster,
-                caption=IMDB_TEXT.format(mention=message.from_user.mention, query=name, title=omdb['title'], trailer=omdb["trailer"], runtime=omdb["runtime"], languages=omdb["languages"], genres=omdb['genres'], year=omdb['year'], rating=omdb['rating'], url=omdb['url'])                   
+                caption=IMDB_TEXT.format(mention=message.from_user.mention, query=name, title=omdb['title'], trailer=omdb["trailer"], runtime=omdb["runtime"], languages=omdb["languages"], genres=omdb['genres'], year=omdb['year'], rating=omdb['rating'], url=omdb['url']),                   
                 #f"<b>🙋🏼 ဟိုင်း  {message.from_user.mention} ရေ.... 🌝🌝\n\n{message.from_user.mention} ရှာတာ 👉🏻 {message.text}👈🏻  ကို မင်မင်ဆီမှရှိတာ ပြပေးထားတယ်နော်။♥️👌...\n\n<b>🙋🏼 Request by : {message.from_user.mention}</b>\n\n<b>⚜️ Join Main Channel \n⚜️ K-Series  👉🏻 @MKSVIPLINK \n⚜️ Movie      👉🏻 @KPMOVIELIST</b>\n</b>⚜️ 𝙐𝙥𝙡𝙤𝙖𝙙𝙚𝙙 𝘽𝙮   : 𝙆𝙤 𝙋𝙖𝙞𝙣𝙜 𝙇𝙖𝙮 🥰</a>",
                 reply_markup=InlineKeyboardMarkup(buttons)
             )
@@ -97,7 +97,7 @@ async def filter(client: Bot, message: Message):
         await client.send_photo(
             chat_id=message.chat.id,
             photo=poster,
-            caption=IMDB_TEXT.format(mention=message.from_user.mention, query=name, title=omdb['title'], trailer=omdb["trailer"], runtime=omdb["runtime"], languages=omdb["languages"], genres=omdb['genres'], year=omdb['year'], rating=omdb['rating'], url=omdb['url'])   
+            caption=IMDB_TEXT.format(mention=message.from_user.mention, query=name, title=omdb['title'], trailer=omdb["trailer"], runtime=omdb["runtime"], languages=omdb["languages"], genres=omdb['genres'], year=omdb['year'], rating=omdb['rating'], url=omdb['url']),  
                 #f"<b>🙋🏼 ဟိုင်း  {message.from_user.mention} ရေ.... 🌝🌝\n\n{message.from_user.mention} ရှာတာ 👉🏻 {message.text}👈🏻  ကို မင်မင်ဆီမှရှိတာ ပြပေးထားတယ်နော်။♥️👌 .\n\n<b>🙋🏼 Request by : {message.from_user.mention}</b>\n\n<b>⚜️ Join Main Channel \n⚜️ K-Series  👉🏻 @MKSVIPLINK \n⚜️ Movie      👉🏻 @KPMOVIELIST</b>\n</b>⚜️ 𝙐𝙥𝙡𝙤𝙖𝙙𝙚𝙙 𝘽𝙮   : 𝙆𝙤 𝙋𝙖𝙞𝙣𝙜 𝙇𝙖𝙮 🥰</a>",
             reply_markup=InlineKeyboardMarkup(buttons)
             ) 
