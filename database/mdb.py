b46a4166a5aae3e98f7d469d94a6afb87ca6e4da
KO_Paing_Lay <107101116+Kopaing12@users.noreply.github.com>
2022-12-21 17:23:30 +0630

    Update mdb.py

--- database/mdb.py
+++ database/mdb.py
@@ -12,7 +12,7 @@ from marshmallow.exceptions import ValidationError
 from config import DATABASE_URI, DATABASE_NAME
 
 
-myclient = MongoClient(DATABASE_URI)
+myclient = (DATABASE_URI)
 mydb = myclient[DATABASE_NAME]
 
 
